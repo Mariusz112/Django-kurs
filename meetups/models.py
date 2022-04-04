d953c6cd8a4fe7e5d0b28b2b98580c9035503a19
Mariusz112 <maniek11744@gmail.com>
2022-04-04 04:19:01 +0200

    p

--- meetups/models.py
+++ meetups/models.py
@@ -1,3 +1,4 @@
+from datetime import date
 from turtle import title
 from django.db import models
 # Create your models here.
@@ -19,11 +20,13 @@ class Participant(models.Model):
 
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
+    organizer_email = models.EmailField()
+    date = models.DateField()
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to='images')
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
-    participants = models.ManyToManyField(Participant, blank=True, null=True)
+    participants = models.ManyToManyField(Participant, blank=True)
 
     def __str__(self):
         return f'{self.title} - {self.slug}'
